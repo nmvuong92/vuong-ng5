e3214190e999096a8522c63ae97be006c1095025
ng-vượng <nmvuong92@gmail.com>
2018-05-07 15:38:26 +0700

    now

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,39 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-
-
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HeroComponent } from './hero/hero.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+const appRoutes: Routes = [
+  { path: 'crisis-center', component: AboutComponent },
+  { path: 'hero/:id',      component: ContactComponent },
+  {
+    path: 'heroes',
+    component: HeroComponent,
+    data: { title: 'Heroes List' }
+  },
+  //mac dinh se chuyen ve /heroes
+  { path: '',
+    redirectTo: '/heroes',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    HeroComponent,
+    PageNotFoundComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     AppRoutingModule
   ],
